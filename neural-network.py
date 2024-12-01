92ec01b5484c7c15cfd5710fb353bd6ae7b10bf4
PanGan21 <ganelis.pan@gmail.com>
2024-12-01 13:08:32 +0200

    add momentum to the optimizer

--- neural-network.py
+++ neural-network.py
@@ -244,11 +244,12 @@ class OptimizerSGD:
 
     # Initialize optimizer - set settings,
     # learning rate of 1. is default for this optimizer
-    def __init__(self, learning_rate=1.0, decay=0.):
+    def __init__(self, learning_rate=1.0, decay=0., momentum=0.):
         self.learning_rate = learning_rate
         self.current_learning_rate = learning_rate
         self.decay = decay
         self.iterations = 0
+        self.momentum = momentum
 
     # Call once before any pamater updates
     def pre_update_params(self):
@@ -258,8 +259,37 @@ class OptimizerSGD:
 
         # Update parameters
     def update_params(self, layer):
-        layer.weights += -self.learning_rate * layer.dweights
-        layer.biases += -self.learning_rate * layer.dbiases
+        # If we use momentum
+        if self.momentum:
+            # If layer does not contain momentum arrays, create them
+            # filled with zeros
+            if not hasattr(layer, 'weight_momentums'):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                # If there is no momentum array for weights
+                # The array doesn't exist for biases yet either.
+                layer.bias_momentums = np.zeros_like(layer.biases)
+            # Build weight updates with momentum - take previous
+            # updates multiplied by retain factor and update with
+            # current gradients
+            weight_updates = \
+                self.momentum * layer.weight_momentums - \
+                self.current_learning_rate * layer.dweights
+            layer.weight_momentums = weight_updates
+            # Build bias updates
+            bias_updates = \
+                self.momentum * layer.bias_momentums - \
+                self.current_learning_rate * layer.dbiases
+            layer.bias_momentums = bias_updates
+        # Vanilla SGD updates (as before momentum update)
+        else:
+            weight_updates = -self.current_learning_rate * \
+                layer.dweights
+            bias_updates = -self.current_learning_rate * \
+                layer.dbiases
+        # Update weights and biases using either
+        # vanilla or momentum updates
+        layer.weights += weight_updates
+        layer.biases += bias_updates
 
     # Call once after any patameter updates
     def post_update_params(self):
@@ -283,7 +313,7 @@ dense2 = LayerDense(64, 3)
 loss_activation = ActivationSoftmaxLossCategoricalCrossentropy()
 
 # Create optimizer
-optimizer = OptimizerSGD(decay=1e-3)
+optimizer = OptimizerSGD(decay=1e-3, momentum=0.9)
 
 # Train in loop
 for epoch in range(10001):
