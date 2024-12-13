3f9368be2f2ecdae669707d3e7ca7f0315650063
PanGan21 <ganelis.pan@gmail.com>
2024-12-13 12:49:13 +0200

    add loss binary cross entropy class

--- neural-network.py
+++ neural-network.py
@@ -235,6 +235,37 @@ class LossCategoricalCrossentropy(Loss):
         self.dinputs = self.dinputs / samples
 
 
+class LossBinaryCrossentropy(Losss):
+    def forward(self, y_pred, y_true):
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # Calculate sample-wise loss
+        sample_losses = -(y_true * np.log(y_pred_clipped) +
+                          (1 - y_true) * np.log(1 - y_pred_clipped))
+        sample_losses = np.mean(sample_losses, axis=1)
+        return sample_losses
+
+    def backward(self, dvalues, y_true):
+        # Number of samples
+        samples = len(dvalues)
+        # Number of outputs in every sample
+        # We will use the first sample to count them
+        outputs = len(dvalues[0])
+
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        clipped_dvalues = np.clip(dvalues, 1e-7, 1 - 1e-7)
+
+        # Calculate gradient
+        self.dinputs = -(y_true / clipped_dvalues -
+                         (1 - y_true) / (1 - clipped_dvalues)) / outputs
+
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+
+
 # Softmax classifier - combined Softmax activation
 # and cross-entropy loss for faster backward step
 class ActivationSoftmaxLossCategoricalCrossentropy():
