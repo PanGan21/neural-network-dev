c558acd1e67c7162ff02e191726d38177ceccb24
PanGan21 <ganelis.pan@gmail.com>
2024-12-28 18:34:20 +0200

    implement mean squared error loss class

--- neural-network.py
+++ neural-network.py
@@ -634,6 +634,41 @@ class LayerDropout:
         self.dinputs = dvalues * self.binary_mask
 
 
+# Linear activation
+class ActivationLinear:
+    # Forward pass
+    def forward(self, inputs):
+        # Just remember values
+        self.inputs = inputs
+        self.output = inputs
+
+      # Backward pass
+    def backward(self, dvalues):
+        # derivative is 1, 1 * dvalues = dvalues - the chain rule
+        self.dinputs = dvalues.copy()
+
+
+# Mean Squared Error loss
+class LossMeanSquaredError(Loss):  # L2 loss
+    # Forward pass
+    def forward(self, y_pred, y_true):
+        # Calculate loss
+        sample_losses = np.mean((y_true - y_pred)**2, axis=-1)
+        return sample_losses
+
+    # Backward pass
+    def backward(self, dvalues, y_true):
+        # Number of samples
+        samples = len(dvalues)
+        # Number of outputs in every sample
+        # We'll use the first sample to count them
+        outputs = len(dvalues[0])
+        # Gradient on values
+        self.dinputs = -2 * (y_true - dvalues) / outputs
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+
+
 # Create dataset
 X, y = spiral_data(samples=1000, classes=2)
 
