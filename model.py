c7c7a790fcba767b2e118cf734ba32c0f3898d3d
PanGan21 <ganelis.pan@gmail.com>
2024-12-29 12:41:07 +0200

    add model class

---
+++ model.py
@@ -0,0 +1,9 @@
+# Model class
+class Model:
+    def __init__(self):
+        # Create a list of network objects
+        self.layers = []
+
+    # Add objects to the model
+    def add(layer):
+        self.layers.append(layer)
