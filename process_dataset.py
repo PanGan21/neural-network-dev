27394b5fc81344165a9c3da09c538cc2db1b0cf2
PanGan21 <ganelis.pan@gmail.com>
2025-01-02 00:08:30 +0200

    load parameters from file in a model

--- process_dataset.py
+++ process_dataset.py
@@ -72,39 +72,8 @@ model.set(loss=LossCategoricalCrossentropy(), optimizer=OptimizerAdam(
 # Finalize the model
 model.finalize()
 
-# Train the model
-model.train(X, y, validation_data=(X_test, y_test),
-            epochs=10, batch_size=128, print_every=100)
-
-# Retrieve model parameters
-parameters = model.get_parameters()
-
-# New model
-# Instantiate the model
-model = Model()
-
-# Add layers
-model.add(LayerDense(X.shape[1], 128))
-model.add(ActivationReLu())
-model.add(LayerDense(128, 128))
-model.add(ActivationReLu())
-model.add(LayerDense(128, 10))
-model.add(ActivationSoftMax())
-
-# Set loss and accuracy objects
-# We do not set optimizer object this time - there's no need to do it
-# as we won't train the model
-model.set(
-    loss=LossCategoricalCrossentropy(),
-    accuracy=AccuracyCategorical())
-
-# Finalize the model
-model.finalize()
-
 # Set model with parameters instead of training it
-model.set_parameters(parameters)
+model.load_parameters('fashion_mnist.parms')
 
 # Evaluate the model
 model.evaluate(X_test, y_test)
-
-model.save_parameters('fashion_mnist.parms')
